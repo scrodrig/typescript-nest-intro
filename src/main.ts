cc8aef87d3a4a0f7cd5d995a02cd0938e4020e0e
Schubert Rodriguez Quezada <schubert.quezada@deutschefintechsolutions.de>
2025-11-03 16:05:10 +0000

    feat(task-0004): Implement PokeApiAdapter and Pokemon class with methods for API interaction

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { setupCounter } from './counter.ts'
 import { name } from './bases/01-types';
 
 import { bulbasaur } from './bases/03-classes.ts';
+import { charmander } from './bases/04-injection.ts';
+console.log("🚀 ~ charmander:", charmander)
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
