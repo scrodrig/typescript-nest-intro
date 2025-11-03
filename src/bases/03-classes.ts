9f4e655d50e7a5ccf54795ab292889148f7d3ffc
Schubert Rodriguez Quezada <schubert.quezada@deutschefintechsolutions.de>
2025-11-03 15:26:14 +0000

    feat(task-0003): Update Pokemon class to include imageUrl getter and refactor getMoves method

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,13 +1,27 @@
+import axios from "axios";
+
 export class Pokemon {
   private readonly id: number;
   private name: string;
   private age?: number;
   private isAlive?: boolean;
+
+  get imageUrl(): string {
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.id}.png`;
+  }
+
   constructor(id: number, name: string, age?: number, isAlive?: boolean) {
     this.id = id;
     this.name = name;
     this.age = age;
     this.isAlive = isAlive;
+  }
+
+  async getMoves(): Promise<string[]> {
+    const response = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${this.id}`
+    );
+    return response.data.moves.map((move: any) => move.move.name);
   }
 }
 
