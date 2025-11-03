9c4c7f94a6c96dcca65975a6af4735b8ea1446e3
Schubert Rodriguez Quezada <schubert.quezada@deutschefintechsolutions.de>
2025-11-03 12:32:05 +0000

    feat(task-0001): Add name and age constants to display in the main application

---
+++ src/bases/01-types.ts
@@ -0,0 +1,7 @@
+export const name = 'scrodrig';
+
+export const age = 30;
+
+
+console.log("🚀 ~ name:", name)
+console.log("🚀 ~ age:", age)
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
+import { name, age } from './bases/01-types';
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
@@ -11,7 +13,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Vite + TypeScript + ${name}</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
